0fcae59d175bedfae028b69877b0a7ee9bd4867c
zuihou <244387066@qq.com>
2024-06-05 15:53:44 +0800

    feat: init

---
+++ src/main/java/top/fsfsfs/boot/common/constant/EchoApi.java
@@ -0,0 +1,22 @@
+package top.fsfsfs.boot.common.constant;
+
+/**
+ * Echo 注解中api的常量
+ * <p>
+ * 切记，该类下的接口和方法，一定要自己手动创建，否则会注入失败
+ * <p>
+ * 本类中的 @fs.generator auto insert 请勿删除
+ *
+ * @author zuihou
+ * @date 2020年01月20日11:16:37
+ */
+public interface EchoApi {
+    // @fs.generator auto insert EchoApi
+
+    /**
+     * 数据字典项 feign查询类 全类名
+     */
+    String DICT_ITEM_FEIGN_CLASS = "dictServiceImpl";
+
+
+}
---
+++ src/main/java/top/fsfsfs/boot/config/CommonAutoConfiguration.java
@@ -0,0 +1,46 @@
+package top.fsfsfs.boot.config;
+
+import cn.hutool.core.util.StrUtil;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.fsfsfs.boot.common.aspect.FsLogAspect;
+import top.fsfsfs.boot.common.cache.CacheKeyModular;
+import top.fsfsfs.boot.config.properties.SystemProperties;
+
+/**
+ * @author tangyh
+ * @version v1.0
+ * @date 2021/9/5 8:04 下午
+ * @create [2021/9/5 8:04 下午 ] [tangyh] [初始创建]
+ */
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@ConditionalOnWebApplication
+@EnableConfigurationProperties(SystemProperties.class)
+public class CommonAutoConfiguration {
+    private final SystemProperties systemProperties;
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = SystemProperties.PREFIX, name = "recordLog", havingValue = "true", matchIfMissing = true)
+    public FsLogAspect getLampLogAspect() {
+        return new FsLogAspect(systemProperties);
+    }
+
+    @PostConstruct
+    public void init() {
+        if (StrUtil.isNotEmpty(systemProperties.getCachePrefix())) {
+            CacheKeyModular.PREFIX = systemProperties.getCachePrefix();
+            log.info("检查到配置文件中：{}.cachePrefix={}", SystemProperties.PREFIX, systemProperties.getCachePrefix());
+        }
+    }
+
+}
