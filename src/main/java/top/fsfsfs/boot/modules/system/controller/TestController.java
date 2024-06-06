fd05c4a3e75f3ca52fee227f475c9bad15de6db8
zuihou <244387066@qq.com>
2024-06-06 11:16:30 +0800

    refactor: fs-cache 删除内存缓存

--- src/main/java/top/fsfsfs/boot/modules/system/controller/TestController.java
+++ src/main/java/top/fsfsfs/boot/modules/system/controller/TestController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.fsfsfs.basic.base.R;
+import top.fsfsfs.basic.cache.redis.CacheResult;
+import top.fsfsfs.basic.cache.repository.CacheOps;
+import top.fsfsfs.basic.model.cache.CacheKey;
 import top.fsfsfs.boot.modules.system.entity.DefGenTestSimple;
 import top.fsfsfs.boot.modules.system.service.TestSimpleService;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/system/test")
@@ -20,11 +25,19 @@ import top.fsfsfs.boot.modules.system.service.TestSimpleService;
 public class TestController {
 
     private final TestSimpleService testSimpleService;
+    private final CacheOps cacheOps;
 
-    @GetMapping("")
-    public R<String> hello() {
+    @PostMapping("/saveCache")
+    public R<Object> saveCache(@RequestBody DefGenTestSimple simple) {
         log.info("hello fs");
-        return R.success("okkk");
+
+        CacheKey key = new CacheKey("tangyh:test", Duration.ofHours(1));
+        cacheOps.set(key, simple);
+
+
+        CacheResult<DefGenTestSimple> result = cacheOps.get(key);
+
+        return R.success(result);
     }
 
     @GetMapping("/list")
