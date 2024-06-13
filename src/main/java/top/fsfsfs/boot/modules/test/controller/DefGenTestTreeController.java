98c6e9d812c11f2dbec823130b4ec494b254fe79
zuihou <244387066@qq.com>
2024-06-14 00:37:16 +0800

    feat: 增加Tree结构方法

--- src/main/java/top/fsfsfs/boot/modules/test/controller/DefGenTestTreeController.java
+++ src/main/java/top/fsfsfs/boot/modules/test/controller/DefGenTestTreeController.java
@@ -2,11 +2,6 @@ package top.fsfsfs.boot.modules.test.controller;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.lang.tree.Tree;
-import cn.hutool.core.lang.tree.TreeNode;
-import cn.hutool.core.lang.tree.TreeNodeConfig;
-import cn.hutool.core.lang.tree.TreeUtil;
-import cn.hutool.core.lang.tree.parser.NodeParser;
-import cn.hutool.core.map.MapUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +17,9 @@ import top.fsfsfs.boot.modules.test.service.DefGenTestTreeService;
 import top.fsfsfs.boot.modules.test.vo.DefGenTestTreeQueryVO;
 import top.fsfsfs.boot.modules.test.vo.DefGenTestTreeResultVO;
 import top.fsfsfs.boot.modules.test.vo.DefGenTestTreeVO;
+import top.fsfsfs.util.utils.FsTreeUtil;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/system/super")
@@ -42,25 +37,10 @@ public class DefGenTestTreeController extends SuperController<DefGenTestTreeServ
         List<DefGenTestTree> list = superService.list();
         List<DefGenTestTreeResultVO> treeList = BeanUtil.copyToList(list, DefGenTestTreeResultVO.class);
 
-        List<Tree<Long>> list3 = TreeUtil.build(treeList, null, TreeNodeConfig.DEFAULT_CONFIG, new FsNodeParser<>());
+        List<Tree<Long>> list3 = FsTreeUtil.build2(treeList);
 
         return success(list3);
     }
 
-    public static class FsNodeParser<T, E extends TreeNode<T>> implements NodeParser<E, T> {
-        @Override
-        public void parse(E treeNode, Tree<T> tree) {
-            tree.setId(treeNode.getId());
-            tree.setParentId(treeNode.getParentId());
-            tree.setWeight(treeNode.getWeight());
-            tree.setName(treeNode.getName());
-
-            //扩展字段
-            final Map<String, Object> extra = treeNode.getExtra();
-            if (MapUtil.isNotEmpty(extra)) {
-                extra.forEach(tree::putExtra);
-            }
-        }
-    }
 
 }
