848ab2ef8c16ba3e6bb705a00a54321690e606fe
hukunzhen <59190089@qq.com>
2024-06-20 22:33:03 +0800

    powerjob集成，并集成了powerjob demo.    maven应用。   powerjob服务器独立运行。版本是<powerjob.version>5.0.1-beta</powerjob.version>

---
+++ src/test/java/top/fsfsfs/boot/powerjob/ClientInitializer.java
@@ -0,0 +1,21 @@
+package top.fsfsfs.boot.powerjob;
+
+import org.junit.jupiter.api.BeforeAll;
+import top.fsfsfs.boot.modules.powerjob.client.IPowerJobClient;
+import top.fsfsfs.boot.modules.powerjob.client.PowerJobClient;
+
+/**
+ * Initialize OhMyClient
+ *
+ * @author tjq
+ * @since 1/16/21
+ */
+public class ClientInitializer {
+
+    protected static IPowerJobClient powerJobClient;
+
+    @BeforeAll
+    public static void initClient() throws Exception {
+        powerJobClient = new PowerJobClient("127.0.0.1:7700", "powerjob-worker-samples", "powerjob123");
+    }
+}
