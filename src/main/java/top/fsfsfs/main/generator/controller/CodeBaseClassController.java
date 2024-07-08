4d76b73cc36f1ddb400cdb70bb80590ceecec03e
zuihou <244387066@qq.com>
2024-07-08 23:59:05 +0800

    feat: 批量查询接口支持排序

--- src/main/java/top/fsfsfs/main/generator/controller/CodeBaseClassController.java
+++ src/main/java/top/fsfsfs/main/generator/controller/CodeBaseClassController.java
@@ -1,17 +1,21 @@
 package top.fsfsfs.main.generator.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+import com.mybatisflex.core.query.QueryWrapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.fsfsfs.basic.base.R;
 import top.fsfsfs.basic.mvcflex.controller.SuperController;
+import top.fsfsfs.basic.mvcflex.utils.ControllerUtil;
 import top.fsfsfs.main.generator.dto.CodeBaseClassDto;
 import top.fsfsfs.main.generator.entity.CodeBaseClass;
 import top.fsfsfs.main.generator.query.CodeBaseClassQuery;
 import top.fsfsfs.main.generator.service.CodeBaseClassService;
 import top.fsfsfs.main.generator.vo.CodeBaseClassVo;
+import top.fsfsfs.util.utils.BeanPlusUtil;
+
+import java.util.List;
 
 /**
  * 基类管理 控制层。
@@ -24,4 +28,12 @@ import top.fsfsfs.main.generator.vo.CodeBaseClassVo;
 @Tag(name = "基类管理接口")
 @RequestMapping("/main/codeBaseClass")
 public class CodeBaseClassController extends SuperController<CodeBaseClassService, Long, CodeBaseClass, CodeBaseClassDto, CodeBaseClassQuery, CodeBaseClassVo> {
+    @Override
+    public R<List<CodeBaseClassVo>> list(CodeBaseClassQuery data) {
+        CodeBaseClass entity = BeanPlusUtil.toBean(data, getEntityClass());
+        QueryWrapper wrapper = QueryWrapper.create(entity, ControllerUtil.buildOperators(entity.getClass()))
+                .orderBy(CodeBaseClass::getWeight, true);
+        List<CodeBaseClass> list = getSuperService().list(wrapper);
+        return success(BeanPlusUtil.toBeanList(list, getVoClass()));
+    }
 }
