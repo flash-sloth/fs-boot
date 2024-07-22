0d2be1abc79e9656ff036227b894bd2ec78bc3d1
liyeo <423770406@qq.com>
2024-07-22 20:22:35 +0800

    feat(sysmenu): 菜单变更为树结构请求

--- src/main/java/top/fsfsfs/main/system/controller/SysMenuController.java
+++ src/main/java/top/fsfsfs/main/system/controller/SysMenuController.java
@@ -1,13 +1,31 @@
 package top.fsfsfs.main.system.controller;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.lang.tree.TreeNode;
+import com.mybatisflex.core.query.QueryWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.fsfsfs.basic.annotation.log.WebLog;
+import top.fsfsfs.basic.base.R;
+import top.fsfsfs.basic.base.entity.BaseEntity;
+import top.fsfsfs.basic.mvcflex.controller.PageController;
 import top.fsfsfs.basic.mvcflex.controller.SuperController;
+import top.fsfsfs.basic.mvcflex.controller.SuperTreeController;
+import top.fsfsfs.basic.mvcflex.utils.ControllerUtil;
 import top.fsfsfs.main.system.entity.SysMenu;
 import top.fsfsfs.main.system.service.SysMenuService;
 import top.fsfsfs.main.system.vo.SysMenuQueryVo;
 import top.fsfsfs.main.system.vo.SysMenuResultVo;
 import top.fsfsfs.main.system.vo.SysMenuVo;
+import top.fsfsfs.util.utils.BeanPlusUtil;
+import top.fsfsfs.util.utils.FsTreeUtil;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * 菜单 控制层。
@@ -17,7 +35,7 @@ import top.fsfsfs.main.system.vo.SysMenuVo;
  */
 @RestController
 @RequestMapping("/main/sysMenu")
-public class SysMenuController extends SuperController<SysMenuService, Long, SysMenu, SysMenuVo, SysMenuQueryVo, SysMenuResultVo> {
+public class SysMenuController extends SuperTreeController<SysMenuService, Long, SysMenu, SysMenuVo, SysMenuQueryVo, SysMenuResultVo> {
 
 
 }
